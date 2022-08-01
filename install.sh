056112db907466f625307799ec6eba066e3e304f
Noor Mohammad Gagguturi <85073575+noor-sec@users.noreply.github.com>
2022-08-01 09:48:08 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -29,7 +29,7 @@ sudo apt-get install nmap -y
 #---------------------------------------------------------------------------------------------------------------------
 #--------------------------------------Installing Go-Lang-------------------------------------------------------------
 #---------------------------------------------------------------------------------------------------------------------
-echo "[+] Installing Go-Lang....."
+echo "[+] Installing Go-Lang....." | lolcat
 wget https://go.dev/dl/go1.18.4.linux-amd64.tar.gz
 cp go1.18.4.linux-amd64.tar.gz /root/
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.18.4.linux-amd64.tar.gz
@@ -39,27 +39,27 @@ go version
 #---------------------------------------------------------------------------------------------------------------------
 #--------------------------------------Installing Nuclei--------------------------------------------------------------
 #---------------------------------------------------------------------------------------------------------------------
-go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest
+go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest | lolcat
 cp /root/go/bin/nuclei /usr/local/bin/
 nuclei
 #---------------------------------------------------------------------------------------------------------------------
 #--------------------------------------Installing SubDomains Finders--------------------------------------------------
 #---------------------------------------------------------------------------------------------------------------------
-echo "[+] Installing Assestfinder..."
+echo "[+] Installing Assestfinder..." | lolcat
 go install github.com/tomnomnom/assetfinder@latest
 cp /root/go/bin/assetfinder /usr/local/bin
-echo "[+] Installing SubFinder......."
+echo "[+] Installing SubFinder......." | lolcat
 go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
 cp /root/go/bin/subfinder /usr/local/bin/
-echo "[+] Installing Findomain........"
+echo "[+] Installing Findomain........" | lolcat
 wget https://github.com/findomain/findomain/releases/latest/download/findomain-linux
 mv findomain-linux findomain
 chmod 777 findomain
 cp findomain /usr/local/bin
-echo "[+] Installing Amass......."
+echo "[+] Installing Amass......." | lolcat
 go install -v github.com/OWASP/Amass/v3/...@master
 cp /root/go/bin/amass /usr/local/bin/
-echo "[+] Installing SubList3r........."
+echo "[+] Installing SubList3r........." | lolcat
 git clone https://github.com/aboul3la/Sublist3r.git 
 mv Sublist3r /opt/
 pip3 install -r /opt/Sublist3r/requirements.txt
@@ -72,28 +72,29 @@ sudo /opt/EyeWitness/Python/setup/setup.sh
 #---------------------------------------------------------------------------------------------------------------------
 #------------------------------------------Installing Url Crawlers ---------------------------------------------------
 #---------------------------------------------------------------------------------------------------------------------
-echo "[+] Installing Waybackurls....."
+echo "[+] Installing Waybackurls....." | lolcat
 go install github.com/tomnomnom/waybackurls@latest
 cp /root/go/bin/waybackurls /usr/local/bin/
-echo "[+] Installing gau - Get All Urls"
+echo "[+] Installing gau - Get All Urls" | lolcat
 go install github.com/lc/gau/v2/cmd/gau@latest
 cp /root/go/bin/gau /usr/local/bin/
-echo "[+] Installing gauplus........"
+echo "[+] Installing gauplus........" | lolcat
 go install github.com/bp0lr/gauplus@latest
 cp /root/go/bin/gauplus /usr/local/bin/
-echo "[+] Installing Paramspider......"
+echo "[+] Installing Paramspider......" | lolcat
 git clone https://github.com/devanshbatham/Paramspider.git
 mv Paramspider /opt/
 pip3 install -r /opt/Paramspider/requirements.txt
 #---------------------------------------------------------------------------------------------------------------------
 #--------------------------------------Installing Subs Live Checker---------------------------------------------------
 #---------------------------------------------------------------------------------------------------------------------
-echo "Installing httpx........"
+echo "Installing httpx........" | lolcat
 go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
 cp /root/go/bin/httpx /usr/local/bin/
 #---------------------------------------------------------------------------------------------------------------------
 #-----------------------------------------Installing SubTakeOver Checkers---------------------------------------------
 #---------------------------------------------------------------------------------------------------------------------
+echo "Installing Subzy........" | lolcat
 go install -v github.com/lukasikic/subzy@latest
 cp /root/go/bin/subzy /usr/local/bin/
 
