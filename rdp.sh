148b59ea804e603a8b78c4be01b4d9139207cbd0
Noor Mohammad Gagguturi <85073575+noor-sec@users.noreply.github.com>
2022-10-29 15:07:13 -0800

    Update and rename aws-rdp.sh to rdp.sh

--- rdp.sh
+++ rdp.sh
@@ -2,7 +2,7 @@ apt-get update -y
 apt-get upgrade -y
 apt install ubuntu-desktop -y
 apt install xrdp -y
-systemctl status xrdp
-adduser xrdp ssl-cert
-sudo ufw allow 3389
-sudo ufw reload
+systemctl enable xrdp
+apt install xfce4 -y
+
+
