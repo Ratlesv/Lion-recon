54f51fd6a52ebad0a758bec5d9d92895da9b80b2
Noor Mohammad Gagguturi <85073575+noor-sec@users.noreply.github.com>
2022-11-10 05:13:37 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,19 +11,19 @@ sudo apt install python-pip -y
 sudo apt install python3-pip -y
 sudo apt install git -y
 sudo apt install php -y
-#sudo apt-get install libcurl4-openssl-dev -y 
-#sudo apt-get install libssl-dev -y
-#sudo apt-get install jq -y
-#sudo apt-get install ruby-full -y
-#sudo apt-get install libcurl4-openssl-dev libxml2 libxml2-dev libxslt1-dev ruby-dev build-essential libgmp-dev zlib1g-dev -y 
-#sudo apt-get install build-essential libssl-dev libffi-dev python-dev -y
-#sudo apt-get install python-setuptools -y
-#sudo apt-get install libldns-dev -y
-#sudo apt-get install python-dnspython -y 
-#sudo apt-get install rename -y
-#sudo apt-get install xargs -y
-#sudo apt install -y awscli -y
-#sudo apt-get install nmap -y
+sudo apt-get install libcurl4-openssl-dev -y 
+sudo apt-get install libssl-dev -y
+sudo apt-get install jq -y
+sudo apt-get install ruby-full -y
+sudo apt-get install libcurl4-openssl-dev libxml2 libxml2-dev libxslt1-dev ruby-dev build-essential libgmp-dev zlib1g-dev -y 
+sudo apt-get install build-essential libssl-dev libffi-dev python-dev -y
+sudo apt-get install python-setuptools -y
+sudo apt-get install libldns-dev -y
+sudo apt-get install python-dnspython -y 
+sudo apt-get install rename -y
+sudo apt-get install xargs -y
+sudo apt install -y awscli -y
+sudo apt-get install nmap -y
 echo "[+] Installing Go-Lang....." 
 wget https://go.dev/dl/go1.19.2.linux-amd64.tar.gz
 cp go1.19.2.linux-amd64.tar.gz /root/
